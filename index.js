6e21703f993c7fc9140c9a0ac1030a69be27f347
BeyondSolar <abhicsjuet@gmail.com>
2023-01-12 17:52:05 +0530

    Updated Hamburger and Responsiveness

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const toggleButton=document.getElementsByClassName('toggle_button')[0]
 const navBar=document.getElementsByClassName('header-inner')[0]
 
-toggleButton.addEventListener('click',()=>{
-    navBar.classList.toggle('active')
+toggleButton.addEventListener('click',(evt)=>{
+    evt.preventDefault();
+    navBar.classList.toggle('active');
 })
